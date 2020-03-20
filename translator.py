07ef42f469d77aeee4e98947ba405f0a9b6dc7f2
SisterAlice <61490967+SisterAlice@users.noreply.github.com>
2020-03-20 11:42:47 +0000

    Update translator.py

--- translator.py
+++ translator.py
@@ -12,18 +12,18 @@ def translate_api_call(text, direction):
     r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate', params={'key': YA_TOKEN, 'text': text, 'lang': direction})
     if r.status_code != 200:
         raise requests.HTTPError
-    return r.json()['text'][0]
+    return r.json()['text'][0] """создана функция с двумя параметрами, создана переменная, достающая запрос при помощи импортированного метода и далее проверка на ошибки """
 
 """Создадим функцию"""
 def translate_text():
   """"""
     current_tab_index = notebook.index(notebook.select())
-    current_tab_object = notebook.winfo_children()[current_tab_index]
-    required_tab_object = notebook.winfo_children()[not(current_tab_index)]
+    current_tab_object = notebook.winfo_children()[current_tab_index]"""здесь используется метод, который образается ко всем дочерним"""
+    required_tab_object = notebook.winfo_children()[not(current_tab_index)]"""а здесь ко всем НЕ дочерним"""
 
     required_tab_object.children['!text'].delete(1.0, tk.END)
 
-    if str(current_tab_object) == '.!notebook.!frame':
+    if str(current_tab_object) == '.!notebook.!frame':  """здесь происходит выбор нужного интерфейса в зависимости от комбинации - преводимый язык - перевод"""
         current_lang, required_lang = 'en', 'ru'
         required_tab_name = '.!notebook.!frame2'
     else:
@@ -33,36 +33,38 @@ def translate_text():
     text = current_tab_object.children['!text'].get(1.0, tk.END).rstrip()
 
     try:
-        result = translate_api_call(text, f'{current_lang}-{required_lang}')
+        result = translate_api_call(text, f'{current_lang}-{required_lang}')"""обращение к апи с обработкой ошибок с помощью форматированной строки"""
     except requests.HTTPError:
         messagebox.showwarning('Warning', 'Yandex.API is not available. Check your network connection')
     else:
         notebook.select(required_tab_name)
-        required_tab_object.children['!text'].insert(1.0, result)
+        required_tab_object.children['!text'].insert(1.0, result)"""вставляет в текстовое поле строку по определенному индексу"""
     
 
-root = tk.Tk()
-root.title('Ya.Translator')
-root.geometry('500x300')
+root = tk.Tk()"""Корневое окно создано. Стоит отметить, что корневое окно является основным окном приложения в нашей программе.
+                 У него присутствуют заголовок и границы, реализованные менеджером окон.
+                 Корневое окно необходимо создавать для любых виджетов."""
+root.title('Ya.Translator')"""Тут название нашего переводчика"""
+root.geometry('500x300')"""задаем параметры"""
 root.resizable(False, False)
 
-notebook = ttk.Notebook(root)
+notebook = ttk.Notebook(root) """Tkinter поддерживает тематизацию виджетов. Тематизированные виджеты можно импортировать из ttk модуля. """
 frame_en = tk.Frame(notebook) """"Здесь родитель не корень а нотебук"""
 frame_ru = tk.Frame(notebook)
-text_en = tk.Text(frame_en) """А хдесь вообще фрейм"""
+text_en = tk.Text(frame_en) """А здесь вообще фрейм"""
 text_ru = tk.Text(frame_ru)
 button = tk.Button(root, text='Translate!', command=translate_text)"""Это мы создаем кнопку,
                                                                    ее родитель корень, она содержит в себе выполнение функции, 
                                                                    которую мы самостоятельно создали выше"""
 
-button.pack(side=tk.BOTTOM, fill=tk.X, expand=True)"""Пакуем кнопку, делаем заполнение"""
+button.pack(side=tk.BOTTOM, fill=tk.X, expand=True)"""Пакуем кнопку, делаем заполнение по горизонтали"""
 frame_en.pack(fill=tk.BOTH)
 frame_ru.pack(fill=tk.BOTH)  """Пакуем все на свете"""
 text_en.pack(fill=tk.BOTH)
 text_ru.pack(fill=tk.BOTH)
 
 notebook.add(frame_en, text='English')
-notebook.add(frame_ru, text='Russian')
+notebook.add(frame_ru, text='Russian')"""метод add  добавляет нужную нам функцию и в данном случае обладает параметром текст и """
 notebook.pack(fill=tk.BOTH)
 
 text_en.focus_set() """Делаем так чтобы можно бло сразу вводить текст без всяких кликов"""
